ef50aa52157c44c424193e5f5bff1f2499d8d191
OleJr <oledjan2014@gmail.com>
2023-10-17 13:21:16 +0300

    Module 2, Exercise 3/3

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -2,23 +2,41 @@ package course.concurrency.m2_async.cf.min_price;
 
 import java.util.Collection;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 public class PriceAggregator {
 
     private PriceRetriever priceRetriever = new PriceRetriever();
 
+    ExecutorService executor = Executors.newCachedThreadPool();
+
     public void setPriceRetriever(PriceRetriever priceRetriever) {
         this.priceRetriever = priceRetriever;
     }
 
-    private Collection<Long> shopIds = Set.of(10l, 45l, 66l, 345l, 234l, 333l, 67l, 123l, 768l);
+    private Collection<Long> shopIds = Set.of(10L, 45L, 66L, 345L, 234L, 333L, 67L, 123L, 768L);
 
     public void setShops(Collection<Long> shopIds) {
         this.shopIds = shopIds;
     }
 
     public double getMinPrice(long itemId) {
-        // place for your code
-        return 0;
+        return shopIds.stream()
+                .map(shopId -> CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
+                        .completeOnTimeout(Double.NaN, 2900, TimeUnit.MILLISECONDS)
+                        .exceptionally(throwable -> Double.NaN))
+                .collect(collectingAndThen(toList(),
+                        futures -> CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                                .thenApply(vd -> futures.stream()
+                                        .map(CompletableFuture::join)
+                                        .min(Double::compareTo)
+                                        .orElse(Double.NaN))
+                                .join()));
     }
 }
