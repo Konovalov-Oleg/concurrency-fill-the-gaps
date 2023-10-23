c08171ebca040e9c4a3045f07f48d5b8b27b4b96
OleJr <oledjan2014@gmail.com>
2023-10-23 18:11:57 +0300

    Ping Pong

--- src/main/java/course/concurrency/m3_shared/PingPong.java
+++ src/main/java/course/concurrency/m3_shared/PingPong.java
@@ -2,17 +2,51 @@ package course.concurrency.m3_shared;
 
 public class PingPong {
 
-    public static void ping() {
+    private final static Object LOCK = new Object();
 
+    private static boolean pong;
+
+    public static void ping() throws InterruptedException {
+        while (true) {
+            synchronized (LOCK) {
+                while (pong) {
+                    LOCK.wait();
+                }
+                System.out.println("Ping");
+                pong = true;
+                LOCK.notify();
+            }
+        }
     }
 
-    public static void pong() {
-
+    public static void pong() throws InterruptedException {
+        while (true) {
+            synchronized (LOCK) {
+                while (!pong) {
+                    LOCK.wait();
+                }
+                System.out.println("Pong");
+                pong = false;
+                LOCK.notify();
+            }
+        }
     }
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(() -> ping());
-        Thread t2 = new Thread(() -> pong());
+        Thread t1 = new Thread(() -> {
+            try {
+                ping();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            try {
+                pong();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
         t1.start();
         t2.start();
     }
