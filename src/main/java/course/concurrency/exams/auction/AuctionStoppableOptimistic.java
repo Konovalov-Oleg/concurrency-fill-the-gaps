4c6bf80e3c951690197b9713a0a725455c8dd7c6
OleJr <oledjan2014@gmail.com>
2023-11-10 21:15:08 +0300

    fix AuctionStoppableOptimistic

--- src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
@@ -13,11 +13,10 @@ public class AuctionStoppableOptimistic implements AuctionStoppable {
     private final AtomicMarkableReference<Bid> latestBid = new AtomicMarkableReference<>(new Bid(0L, 0L, Long.MIN_VALUE), false);
 
     public boolean propose(Bid bid) {
-        boolean[] markHolder = new boolean[1];
         Bid latestValue;
         do {
-            latestValue = latestBid.get(markHolder);
-            if ((bid.getPrice() <= latestValue.getPrice()) || markHolder[0]) {
+            latestValue = latestBid.getReference();
+            if ((bid.getPrice() <= latestValue.getPrice()) || latestBid.isMarked()) {
                 return false;
             }
         } while (!latestBid.compareAndSet(latestValue, bid, false, false));
@@ -31,10 +30,9 @@ public class AuctionStoppableOptimistic implements AuctionStoppable {
     }
 
     public Bid stopAuction() {
-        boolean[] markHolder = new boolean[1];
         Bid latestValue;
         do {
-            latestValue = latestBid.get(markHolder);
+            latestValue = latestBid.getReference();
         } while (!latestBid.attemptMark(latestValue, true));
         return latestValue;
     }
